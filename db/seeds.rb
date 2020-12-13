042447f83da883f48495537b63582bc8b2f302f2
danajackson2 <danajackson2@gmail.com>
2020-12-12 18:07:01 -0800

    yay

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title:"Hey there", description:"Best articel aof the amls")
+puts "seeded dat file yo"
